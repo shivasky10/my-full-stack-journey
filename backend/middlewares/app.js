9494080b13d9f032103ea8887b21c59a80a79084
shivasky10 <sky10shiva@gmail.com>
2025-07-25 22:37:27 +0530

    backend

--- backend/middlewares/app.js
+++ backend/middlewares/app.js
@@ -1,6 +1,38 @@
 const express=require("express");
 const app = express();
 
+// --------middlewares --------------
+
+app.use((req,res,next)=>{
+    console.log("iam 1st middleware");
+    next()
+});
+
+
+app.use("/api",(req,res,next)=>{
+    const {token}=req.query;
+    if(token=="giveaccess"){
+        next();
+    }
+    res.send("access denied");
+})
+app.use("/api",(req,res)=>{
+    res.send("data");
+})
+// app.use((req,res,next)=>{
+//     console.log("iam 2nd middleware");
+//     next()
+// });
+
+// logger--------------
+// app.use((req,res)=>{
+//     req.time=new Date(Date.now()).toString();
+//     console.log(req.method , req.path,req.time)
+// })
+
+app.get("/",(req,res)=>{
+    res.send("iam root")
+})
 
 app.listen(8080,()=>{
     console.log("listening on port 8080");
