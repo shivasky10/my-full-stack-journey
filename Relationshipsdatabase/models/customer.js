7fe14a42effad60af9ef42a0127dbccc99d12906
shivasky10 <sky10shiva@gmail.com>
2025-07-29 22:49:57 +0530

    db relations

---
+++ Relationshipsdatabase/models/customer.js
@@ -0,0 +1,54 @@
+const mongoose=require("mongoose");
+const {Schema}=mongoose;
+
+main().then(()=>{console.log("connection successful")}).catch(err => console.log(err));
+async function main() {
+  await mongoose.connect('mongodb://127.0.0.1:27017/relationDemo');
+}
+
+const orderSchema = new Schema({
+    item:String,
+    price:Number,
+});
+
+const customerSchema = new Schema({
+     name :String,
+     Orders :[
+    {
+        type : Schema.Types.ObjectId,
+        ref:"order"
+    }]
+});
+
+
+
+const Order = mongoose.model("Order",orderSchema);
+const Customer = mongoose.model("Customer",customerSchema);
+
+const addCustomer = async()=>{
+    let customer1 = new Customer({
+        name :"shiva",
+    });
+
+    let order1 =await Order.findOne({item:"chips"});
+    let order2 =await Order.findOne({item:"chocolate"});
+
+    customer1.Orders.push(order1);
+    customer1.Orders.push(order2);
+
+    let result= await customer1.save();
+    console.log(result);
+}
+
+addCustomer();
+
+// const addorders = async()=>{
+//     let res =await Order.insertMany([
+//         {item:"samosa",price:15},
+//         {item:"chips",price:10},
+//         {item:"chocolate",price:40}
+//     ]);
+//     console.log(res);
+// };
+
+// addorders();
