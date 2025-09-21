5dbff4f3db8c3fdd3479c06b885918e52dbfb99b
Shiva Kumar Yadav <160407079+shivasky10@users.noreply.github.com>
2025-09-21 13:18:42 +0530

    react

--- react/react-state/src/App.jsx
+++ react/react-state/src/App.jsx
@@ -6,13 +6,11 @@ import Todolist from './TodoList'
 // import Counter from './counter'
 // import LikeButtton from './LikeButton'
 import Lottery from './Lottery'
-import Ticket from './Ticket'
 
 function App() {
   return(
   <>
-  <Ticket ticket={[0,5,4]}/>
-  <Ticket ticket={[4,5,2,3,1]}/>
+    <Lottery n={3} winningSum={15}/>
   </>
   )
 }
--- react/react-state/src/Lottery.jsx
+++ react/react-state/src/Lottery.jsx
@@ -1,23 +1,19 @@
 import { useState } from "react"
 import "./Lottery.css"
 import { genTicket,sum } from "./helper";
+import Ticket from "./Ticket";
 
-export default function Lottery(){
-    let [ticket,setTicket]=useState(genTicket(3));
-    let isWinning = sum(ticket)=== 15;
+export default function Lottery({n,winningSum}){
+    let [ticket,setTicket]=useState(genTicket(n));
+    let isWinning = sum(ticket)=== winningSum;
 
     let buyTicket= ()=>{
-        setTicket(genTicket(3));
+        setTicket(genTicket(n));
     }
     return(
         <div>
             <h1>Lottery game !</h1>
-            <div className="ticket">
-                <span>{ticket[0]}</span>
-                <span>{ticket[1]}</span>
-                <span>{ticket[2]}</span>
-            </div>
-            <br></br>
+            <Ticket ticket={ticket}/>
             <button onClick={buyTicket}>Buy new Ticket</button>
             <h3>{isWinning && "congrats You won"}</h3>
         </div>
